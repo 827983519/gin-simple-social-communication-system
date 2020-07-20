294c950dd6e76ae06950a2ddd952d1273a5424b6
yongjian.zeng <yongjian.zeng@shopee.com>
2020-07-20 11:10:01 +0800

    add validator

---
+++ apps/example/services/login/login_services.go
@@ -0,0 +1,43 @@
+package login
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	grpc_login "sbs-entrytask-template/apps/example/grpc/proto/login_proto"
+	Validator "sbs-entrytask-template/apps/example/services/validator"
+	"sbs-entrytask-template/libs/response"
+	"sbs-entrytask-template/libs/response/Errorcode"
+)
+
+
+const (
+	address  = "localhost:50051"
+)
+
+
+
+func Login(c *gin.Context){
+	data, err := Validator.CheckLoginForm(c)
+	if err != nil {
+		response.Response_data(c, gin.H{}, Errorcode.PARAMS_VALIDATE_WRONG, "Parameter verification failed.")
+	}
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		response.Response_data(c, gin.H{}, Errorcode.CONNECT_GRPC_FAILED, "Cannot connect grpc sever.")
+	}
+	defer conn.Close()
+	client := grpc_login.NewLoginServiceClient(conn)
+
+	r, _ := client.Login(context.Background(), &grpc_login.LoginRequest{
+				Username:  data.Username,
+				Password:  data.Password,
+	})
+	if r.Retcode != nil {
+		response.Response_data(c, gin.H{}, r.Retcode.Errorcode, r.Retcode.Message)
+		return
+	}
+
+	response.Response_data(c, gin.H{}, Errorcode.OK, "")
+}
